ff3f9afa0dc6baf563808e413f2655cc9893da3a
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-20 19:41:49 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/webigode/cursospringionic/service/CategoriaService.java
@@ -0,0 +1,27 @@
+package com.webigode.cursospringionic.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webigode.cursospringionic.domain.Categoria;
+import com.webigode.cursospringionic.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
+	}
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
