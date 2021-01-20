3ebb8683f35cf9e9dc31c0f3824b7ca1ed3edbd4
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-20 18:35:09 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/webigode/cursospringionic/resources/CategoriaResource.java
+++ src/main/java/com/webigode/cursospringionic/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.webigode.cursospringionic.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.webigode.cursospringionic.domain.Categoria;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		List<Categoria> list = new ArrayList<>();
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		list.addAll(Arrays.asList(cat1,cat2));
+		
+		return list;
 	}
 }
