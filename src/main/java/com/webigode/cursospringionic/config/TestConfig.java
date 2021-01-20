cd3549ed88d215895c3c7bb0abe37b6a7cb787ce
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-20 20:05:29 -0300

    Criando operação de instanciação e perfil de teste

---
+++ src/main/java/com/webigode/cursospringionic/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.webigode.cursospringionic.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.webigode.cursospringionic.domain.Categoria;
+import com.webigode.cursospringionic.repositories.CategoriaRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+
+}
