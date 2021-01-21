e1c5e80aaf0d65fbbb10e78142c977f018f751e7
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-21 16:21:32 -0300

    Estado e Cidade

--- src/main/java/com/webigode/cursospringionic/config/TestConfig.java
+++ src/main/java/com/webigode/cursospringionic/config/TestConfig.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.webigode.cursospringionic.domain.Categoria;
+import com.webigode.cursospringionic.domain.Cidade;
+import com.webigode.cursospringionic.domain.Estado;
 import com.webigode.cursospringionic.domain.Produto;
 import com.webigode.cursospringionic.repositories.CategoriaRepository;
+import com.webigode.cursospringionic.repositories.CidadeRepository;
+import com.webigode.cursospringionic.repositories.EstadoRepository;
 import com.webigode.cursospringionic.repositories.ProdutoRepository;
 
 @Configuration
@@ -21,6 +25,12 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -42,6 +52,18 @@ public class TestConfig implements CommandLineRunner{
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));		
 	}
 
 }
---
+++ src/main/java/com/webigode/cursospringionic/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.webigode.cursospringionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.webigode.cursospringionic.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
