7a9fde6f55f6f082ebe27c64897eaf1152f9db86
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-24 15:25:43 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/webigode/cursospringionic/resources/CategoriaResource.java
+++ src/main/java/com/webigode/cursospringionic/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -58,5 +60,16 @@ public class CategoriaResource {
 	public ResponseEntity<Void> deleteById(@PathVariable Integer id){
 		service.deleteById(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0")Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="name")String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC")String direction){
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(x -> new CategoriaDTO(x));
+		return ResponseEntity.ok().body(listDto);
 	}
 }
