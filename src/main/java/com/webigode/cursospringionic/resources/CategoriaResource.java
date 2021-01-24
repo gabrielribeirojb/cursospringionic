4ed483a5815bd99eba4cefb5e76c61de84d0f2da
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-24 13:45:51 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/webigode/cursospringionic/resources/CategoriaResource.java
+++ src/main/java/com/webigode/cursospringionic/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.webigode.cursospringionic.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webigode.cursospringionic.domain.Categoria;
+import com.webigode.cursospringionic.dto.CategoriaDTO;
 import com.webigode.cursospringionic.service.CategoriaService;
 
 @RestController
@@ -23,6 +26,13 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(x -> new CategoriaDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
