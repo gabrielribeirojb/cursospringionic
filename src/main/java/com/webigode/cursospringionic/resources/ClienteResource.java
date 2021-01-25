33d13e71ca45d85a628766f4e453f782c43256a4
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-25 16:53:32 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/webigode/cursospringionic/resources/ClienteResource.java
+++ src/main/java/com/webigode/cursospringionic/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.webigode.cursospringionic.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webigode.cursospringionic.domain.Cliente;
 import com.webigode.cursospringionic.dto.ClienteDTO;
+import com.webigode.cursospringionic.dto.ClienteNewDTO;
 import com.webigode.cursospringionic.service.ClienteService;
 
 @RestController
@@ -41,13 +45,13 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
-//	@PostMapping
-//	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto) {
-//		Cliente obj = service.fromDTO(objDto);
-//		obj = service.insert(obj);
-//		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-//		return ResponseEntity.created(uri).build();
-//	}
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
--- src/main/java/com/webigode/cursospringionic/service/ClienteService.java
+++ src/main/java/com/webigode/cursospringionic/service/ClienteService.java
@@ -3,6 +3,8 @@ package com.webigode.cursospringionic.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.webigode.cursospringionic.domain.Cidade;
 import com.webigode.cursospringionic.domain.Cliente;
+import com.webigode.cursospringionic.domain.Endereco;
+import com.webigode.cursospringionic.domain.enums.TipoCliente;
 import com.webigode.cursospringionic.dto.ClienteDTO;
+import com.webigode.cursospringionic.dto.ClienteNewDTO;
 import com.webigode.cursospringionic.repositories.ClienteRepository;
+import com.webigode.cursospringionic.repositories.EnderecoRepository;
 import com.webigode.cursospringionic.service.excetions.DataIntegrityException;
 import com.webigode.cursospringionic.service.excetions.ObjectNotFoundException;
 
@@ -21,6 +28,9 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
@@ -41,6 +51,14 @@ public class ClienteService {
 		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 
 	public void deleteById(Integer id) {
 		findById(id);
@@ -54,6 +72,25 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getName(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getName(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.valueOf(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogadouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
