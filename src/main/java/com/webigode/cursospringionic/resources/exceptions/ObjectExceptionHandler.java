ac0a37d56408751c5e608c1f904f38d18c831d21
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-25 12:23:06 -0300

    Personalizando o retorno da validação

--- src/main/java/com/webigode/cursospringionic/resources/exceptions/ObjectExceptionHandler.java
+++ src/main/java/com/webigode/cursospringionic/resources/exceptions/ObjectExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,6 +30,19 @@ public class ObjectExceptionHandler {
 		String error = "Não se pode excluir um objeto que tenha outros objetos associados a ele";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.ok().body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		String error ="Erro na validação do campo";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ValidationError err = new ValidationError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		
 		return ResponseEntity.ok().body(err);
 	}
 	
--- src/main/java/com/webigode/cursospringionic/resources/exceptions/StandardError.java
+++ src/main/java/com/webigode/cursospringionic/resources/exceptions/StandardError.java
@@ -1,10 +1,13 @@
 package com.webigode.cursospringionic.resources.exceptions;
 
+import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class StandardError {
+public class StandardError implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant timestamp;
