1d52c080911a894e5d9ce3480b2b2962f7da2baf
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-26 16:17:00 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/webigode/cursospringionic/dto/ClienteDTO.java
+++ src/main/java/com/webigode/cursospringionic/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.webigode.cursospringionic.domain.Cliente;
+import com.webigode.cursospringionic.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/webigode/cursospringionic/service/validation/ClienteUpdateValidator.java
@@ -0,0 +1,56 @@
+package com.webigode.cursospringionic.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.webigode.cursospringionic.domain.Cliente;
+import com.webigode.cursospringionic.dto.ClienteDTO;
+import com.webigode.cursospringionic.repositories.ClienteRepository;
+import com.webigode.cursospringionic.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		//Lista vazia de FieldMessage para adicionar erros referentes aos campos e suas mensagens
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repository.findByEmail(objDto.getEmail());
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		//Percorre lista de FieldMessage e inserindo erro corresponte para a lista de erros do framework
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		
+		//Se não houver nenhum erro, a validação foi bem sucedida
+		return list.isEmpty();
+	}
+}
