d2ec70b611eba4c67053e6149908fa51154078e8
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-27 18:50:23 -0300

    Criando profile de desenvolvimento e ajustando profile de teste

--- src/main/java/com/webigode/cursospringionic/config/DevConfig.java
+++ src/main/java/com/webigode/cursospringionic/config/DevConfig.java
@@ -3,22 +3,30 @@ package com.webigode.cursospringionic.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-
 import com.webigode.cursospringionic.service.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
-	public boolean instantiateDatabase() throws ParseException {
+	public boolean instantiateDatabase() throws ParseException{
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
 		dbService.instantiateTestDatabase();
 		return true;
 	}
