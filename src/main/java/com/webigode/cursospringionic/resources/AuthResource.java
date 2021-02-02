ee6a9f3d1b386fa9b887c0e717d10bd199c0d460
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-02 16:20:08 -0300

    Esqueci a senha

--- src/main/java/com/webigode/cursospringionic/resources/AuthResource.java
+++ src/main/java/com/webigode/cursospringionic/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.webigode.cursospringionic.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.webigode.cursospringionic.dto.EmailDTO;
 import com.webigode.cursospringionic.security.JWTUtil;
 import com.webigode.cursospringionic.security.UserSS;
+import com.webigode.cursospringionic.service.AuthService;
 import com.webigode.cursospringionic.service.UserService;
 
 @RestController
@@ -19,6 +23,9 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	@PostMapping(value = "/refresh_token")
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
@@ -26,5 +33,10 @@ public class AuthResource {
 		response.addHeader("Authorization", "Bearer " + token);
 		return ResponseEntity.noContent().build();
 	}
-
+	
+	@PostMapping(value = "/forgot")
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
+	}
 }
