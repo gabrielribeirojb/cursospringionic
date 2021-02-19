a0d86380a591cca88977188c37d4b528ca8bb9da
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-19 18:31:57 -0300

    Enviando imagem via endpoint

--- src/main/java/com/webigode/cursospringionic/CursospringionicApplication.java
+++ src/main/java/com/webigode/cursospringionic/CursospringionicApplication.java
@@ -1,17 +1,11 @@
 package com.webigode.cursospringionic;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.webigode.cursospringionic.service.S3Service;
-
 @SpringBootApplication
 public class CursospringionicApplication implements CommandLineRunner{
-
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringionicApplication.class, args);
@@ -19,7 +13,6 @@ public class CursospringionicApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\fotos\\institucional-banner.jpg");
 	}
 
 }
--- src/main/java/com/webigode/cursospringionic/service/ClienteService.java
+++ src/main/java/com/webigode/cursospringionic/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.webigode.cursospringionic.service;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.webigode.cursospringionic.domain.Cidade;
 import com.webigode.cursospringionic.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente findById(Integer id) {
 		
@@ -110,5 +115,9 @@ public class ClienteService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
