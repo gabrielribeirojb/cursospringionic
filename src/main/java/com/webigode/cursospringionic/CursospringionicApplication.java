9cfd2defb133c46c5725a51a8347017d9d00e24f
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-19 16:01:22 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/webigode/cursospringionic/CursospringionicApplication.java
+++ src/main/java/com/webigode/cursospringionic/CursospringionicApplication.java
@@ -1,13 +1,25 @@
 package com.webigode.cursospringionic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringionicApplication {
+import com.webigode.cursospringionic.service.S3Service;
 
+@SpringBootApplication
+public class CursospringionicApplication implements CommandLineRunner{
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringionicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\fotos\\institucional-banner.jpg");
 	}
 
 }
