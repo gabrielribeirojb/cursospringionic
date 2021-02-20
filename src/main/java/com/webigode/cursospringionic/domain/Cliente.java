3d7a63eb3d87c3dc5274e9dc24d468118e994778
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-20 16:15:20 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/webigode/cursospringionic/domain/Cliente.java
+++ src/main/java/com/webigode/cursospringionic/domain/Cliente.java
@@ -55,8 +55,6 @@ public class Cliente implements Serializable{
 	@JsonIgnore
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -151,15 +149,7 @@ public class Cliente implements Serializable{
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
 	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}	
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
